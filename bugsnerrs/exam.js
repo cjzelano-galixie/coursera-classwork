e6e61fae3e5d121759e2eea3a969e3d6d0d89edb
Chloe <cjzelano@gmail.com>
2026-05-29 17:05:25 -0400

    This is the worksheet to check answers for the exam

---
+++ bugsnerrs/exam.js
@@ -0,0 +1,31 @@
+// try {
+//   throw new Error();
+//   console.log("Hello");
+// } catch (err) {
+//   console.log("Goodbye");
+// }
+
+// function add(a, b) {
+//   var result = a + b;
+//   console.log(result);
+// }
+
+// add(3, "4");
+
+// var str = "Hello";
+// console.log(str.match("jello"));
+
+// try {
+//   Number(5).toPrecision(300);
+// } catch (e) {
+//   console.log("There was an error");
+// }
+
+// var burger = ["bun", "beef", "lettuce", "tomato sauce", "onion", "bun"];
+// console.log(burger[2]);
+
+// try {
+// console.log('hello)
+// } catch(e) {
+// console.log('caught')
+// }
