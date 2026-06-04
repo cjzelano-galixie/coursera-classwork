4f15cc4ea511a8fd85609ffd97533d2083c15a16
Chloe <cjzelano@gmail.com>
2026-06-04 17:34:50 -0400

    this is the final example before the lab

--- ProgrammingParadigms/varletconst.js
+++ ProgrammingParadigms/varletconst.js
@@ -52,3 +52,42 @@
 
 //         var   |  let   |  const
 // Lenient<----------------------->Strict
+
+// Testing
+
+//__________________________________________
+// var globalVar = 77;
+
+// function scopeTest() {
+//   var localVar = 88;
+// }
+
+// console.log(localVar); // ReferenceError: localVar is not defined
+
+//__________________________________________
+// function meal(animal) {
+//   animal.food = animal.food + 10;
+// }
+
+// var dog = {
+//   food: 10,
+// };
+// meal(dog);
+// meal(dog);
+
+// console.log(dog.food); // expected response: 30
+
+//__________________________________________
+// function two() {
+//   return 2;
+// }
+
+// function one() {
+//   return 1;
+// }
+
+// function calculate(initialValue, incrementValue) {
+//   return initialValue() + incrementValue() + incrementValue();
+// }
+
+// console.log(calculate(two, one)); // expected response 4
